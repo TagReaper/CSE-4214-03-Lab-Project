99c9a854a1348ef85d5a660493685289a0f8a47e
TagReaper <coxlucas2022@gmail.com>
2025-09-17 22:33:45 -0500

    Formatted and setup Navigation Bar

--- thrift-u/app/layout.tsx
+++ thrift-u/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from 'next/link';
+import Image from "next/image";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,11 +30,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div style={{backgroundColor: "black", alignItems: 'center', gap: '5px', outlineColor: '#c5c5c5', outlineStyle: 'solid', outlineWidth: '3px'}}>
+          <span style={{display: 'inline-flex', padding: '5px', gap:'5px', alignItems:'center'}}>
+            <Image className="dark:invert" src="/Icons/home.svg" alt="Home logo" width={30} height={30} />
+            <Link href = {"/"}> Home </Link>
+          </span>
+          <span style={{display: 'inline-flex', padding: '5px', gap:'5px', alignItems:'center'}}>
+            <Image className="dark:invert" src="/Icons/buyer.svg" alt="Home logo" width={30} height={30} />
+            <Link href = {"/login"}> Login </Link>
+          </span>
+          <span style={{display: 'inline-flex', padding: '5px', gap:'5px', alignItems:'center'}}>
+            <Image className="dark:invert" src="/Icons/seller.svg" alt="Home logo" width={30} height={30} />
+            <Link href = {"/signup"}> Sign-Up </Link>
+          </span>
+        </div>
         <div>
           {" "}
-          <Link href = {"/"}> Home </Link>
-          <Link href = {"/login"}> Login </Link>
-          <Link href = {"/signup"}> Sign-Up </Link>
         </div>
         {children}
       </body>
