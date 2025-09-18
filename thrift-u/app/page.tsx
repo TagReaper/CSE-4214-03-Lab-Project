3313c099d7341bee8562764614e275985da0a9e7
TagReaper <coxlucas2022@gmail.com>
2025-09-18 17:40:17 -0500

    Added CSS Styling and Added ThriftU Logo

--- thrift-u/app/page.tsx
+++ thrift-u/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
-  return <div> Home Page </div>
+  return <div className="center">
+      <Image style={{filter:'invert(100%)'}}src="/Graphics/ThriftULogoModern.png" alt="ThriftU Logo" width={400} height={400}/>
+      Home Page
+      </div>
 }
