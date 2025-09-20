c79b3390fe02b39199575cb5e06159eb518d37e1
TagReaper <coxlucas2022@gmail.com>
2025-09-19 22:40:13 -0500

    Created Login Page and Form & added components

--- thrift-u/app/(auth)/login/page.tsx
+++ thrift-u/app/(auth)/login/page.tsx
@@ -2,5 +2,25 @@ import Image from "next/image";
 
 
 export default function Login() {
-    return <div> Login Page </div>;
+    return <div className="login-box">
+        <h1>
+            Login
+        </h1>
+        <form action={"/"}>
+            <div className="grid">
+                <label className="font-bold"> Email: </label>
+                <input className="input-box" type="email" required></input>
+            </div>
+            <div className="grid">
+                <label className="font-bold"> Password: </label>
+                <input className="input-box" type="password" required></input>
+            </div>
+            <div className="grid grid-cols-2">
+                <input className="button" type="submit" value={"Login"}></input>
+                <form action={"/signup"}>
+                    <input style={{float: "right"}} className="button" type="submit" value={"Sign-Up"}></input>
+                </form>
+            </div>
+        </form>
+    </div>
 }
