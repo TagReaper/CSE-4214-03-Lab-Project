ed05cfcc71e21f1242dc41947337cdb25a2ef978
revcane <xrengamer@gmail.com>
2025-10-11 21:32:40 -0500

    Added set up for middleware

--- thrift-u/firebase/clientApp.ts
+++ thrift-u/firebase/clientApp.ts
@@ -1,21 +1,21 @@
-import firebase from "firebase/app"
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "@firebase/firestore";
 import "firebase/auth"
 import "firebase/firestore"
-import { getAnalytics } from "firebase/analytics";
+import { initialize } from "next/dist/server/lib/render-server";
+import { firebaseConfig } from "firebase-functions/v1";
 
-const clientCredentials = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SEND_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+export const clientCredentials = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SEND_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-if (!firebase.getApps.length) {
-    const app = firebase.initializeApp(clientCredentials);
-    const analytics = getAnalytics(app);
-}
+const app = initializeApp(clientCredentials);
+const db = getFirestore(app)
 
-export default firebase;
+export default db
