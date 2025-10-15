f400b9bef4c5ed3c1e6c9ed6aef1a5162092b809
TagReaper <coxlucas2022@gmail.com>
2025-10-14 22:49:05 -0500

    Created Ban Button functionality

--- thrift-u/components/banUser.js
+++ thrift-u/components/banUser.js
@@ -1,11 +1,29 @@
 'use client'
 
 import db from '../firebase/clientApp'
-import {doc, getDoc} from '@firebase/firestore'
+import {doc, getDoc, updateDoc} from '@firebase/firestore'
 
 const BanUser = ({access, id}) => {
+    const handleBan = async () => {
+        if(access == 1){
+            const docRef = await getDoc(doc(db, "Seller", id))
+            await updateDoc(docRef, {
+                banned: true
+            })
+        } else if(access == 2){
+            const docRef = await getDoc(doc(db, "Buyer", id))
+            await updateDoc(docRef, {
+                banned: true
+            })
+        }
+    }
 
+    return (<div>
+        <button onClick={handleBan} className="w-20 border-2 border-black rounded-2xl font-bold font-stretch-100% bg-red-500 text-white" >
+            BAN
+        </button>
+    </div>
+    )
 }
 
-
-<button className="w-20 border-2 border-black rounded-2xl font-bold font-stretch-100% bg-red-500 text-white" >BAN</button>
+export default BanUser
