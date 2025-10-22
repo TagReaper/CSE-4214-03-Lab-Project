8ce5d39d9848e5fc83aca8bc10aa01f4d07a618b
revcane <xrengamer@gmail.com>
2025-10-21 22:22:12 -0500

    Created Firebase functions for account creation and seller approval

--- thrift-u/components/signUp.js
+++ thrift-u/components/signUp.js
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react';
-import { createUserWithEmailAndPassword, validatePassword  } from "firebase/auth";
-import FireData from '../firebase/clientApp'
-import { collection, addDoc, doc, setDoc } from '@firebase/firestore';
-import { useRouter }  from 'next/navigation'
+import { useState, useEffect } from "react";
+import { validatePassword } from "firebase/auth";
+import FireData from "../firebase/clientApp";
+import { httpsCallable } from "firebase/functions";
+import { useRouter } from "next/navigation";
 
 const UIPasswordValidation = (password) => {
     return {
@@ -17,9 +17,12 @@ const UIPasswordValidation = (password) => {
 };
 
 const SignUp = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPass] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPass] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [firstName, setFirst] = useState("");
+    const [lastName, setLast] = useState("");
+    const [sellerReq, setSeller] = useState(false);
     const [validation, setValidation] = useState({
         minLength: false,
         hasLowercase: false,
@@ -28,20 +31,18 @@ const SignUp = () => {
         hasSpecialChar: false,
         isValid: false,
     });
-    const [firstName, setFirst] = useState('');
-    const [lastName, setLast] = useState('');
-    const [sellerReq, setSeller] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
-    const serverTime = new Date();
-    const router = useRouter()
+    const [error, setError] = useState("");
+    const router = useRouter();
 
     useEffect(() => {
         setValidation(UIPasswordValidation(password));
     }, [password]);
     const handleSubmit = async (event) => {
         event.preventDefault();
-        setError('');
+        setError("");
+
+        const role = sellerReq ? "seller" : "buyer";
 
         if (password !== confirmPassword) {
             setError("Passwords do not match.");
@@ -49,113 +50,168 @@ const SignUp = () => {
         }
         try {
             setLoading(true);
-            const passwordValidationStatus = await validatePassword(FireData.auth, password);
-            if (!passwordValidationStatus.isValid) {
-                setError("Password does not meet the security policy.");
-                setLoading(false);
-                return;
+            if (process.env.NODE_ENV === "production") {
+                const passwordValidationStatus = await validatePassword(
+                    FireData.auth,
+                    password,
+                );
+                if (!passwordValidationStatus.isValid) {
+                    setError("Password does not meet the security policy.");
+                    setLoading(false);
+                    return;
+                }
             }
-            const userCredential = await createUserWithEmailAndPassword(FireData.auth, email, password);
-            const user = userCredential.user;
-            console.log('account created', user.uid);
-            await setDoc(doc(FireData.db, 'User', user.uid), {
-                email: email,
-                firstName: firstName,
-                lastName: lastName,
-                accessLevel: sellerReq ? 1 : 2,
-                dateCreated: serverTime.toLocaleString(),
-                deletedAt: "",
+
+            console.log("Client Payload:", {
+                email,
+                password,
+                role,
+                firstName,
+                lastName,
             });
 
+            const registerUser = httpsCallable(
+                FireData.functions,
+                "registerUser",
+            );
+            await registerUser({
+                email,
+                password,
+                role,
+                firstName,
+                lastName,
+            });
+
+            const userCredential = await signInWithEmailAndPassword(
+                FireData.auth,
+                email,
+                password,
+            );
+            const user = userCredential.user;
+            console.log("account created and signed in:", user.uid);
+
             if (sellerReq) {
-                const confirmed = confirm("Are you sure you want to request a seller account?");
+                const confirmed = confirm(
+                    "Are you sure you want to request a seller account?",
+                );
                 if (confirmed) {
-                await addDoc(collection(FireData.db, 'Seller'), {
-                    UserID: user.uid,
-                    banned: false,
-                    validated: false,
-                    Flags: 0,
-                });
-                console.log('Seller record created');
+                    console.log("Seller record created");
                 } else {
-                setSeller(false);
+                    setSeller(false);
                 }
-            } else {
-                await addDoc(collection(FireData.db, 'Buyer'), {
-                UserID: user.uid,
-                banned: false,
-                address: "",
-                city: "",
-                state: "",
-                zip: "",
-                numOrders: 0,
-                });
-                console.log('Buyer record created');
             }
 
-            alert('Account created successfully');
-            router.push('/login');
-
+            alert("Account created successfully");
+            router.push("/"); // redirect to home page
         } catch (error) {
-        console.error('error creating account:', error);
-        switch (error.code) {
-        case 'auth/email-already-in-use':
-            setError("This email address is already registered to an account.");
-            break;
-        case 'auth/invalid-email':
-            setError("Email address is invalid.");
-            break;
-        case 'auth/weak-password':
-            setError("Password is too weak. Please choose a stronger password.");
-            break;
-        default:
-            setError("Error creating account, please try again. " + error.code);
+            console.error("error creating account:", error);
+            switch (error.code) {
+                case "auth/email-already-in-use":
+                    setError(
+                        "This email address is already registered to an account.",
+                    );
+                    break;
+                case "auth/invalid-email":
+                    setError("Email address is invalid.");
+                    break;
+                case "auth/weak-password":
+                    setError(
+                        "Password is too weak. Please choose a stronger password.",
+                    );
+                    break;
+                default:
+                    setError(
+                        "Error creating account, please try again. " +
+                            error.code,
+                    );
             }
-        }
-        finally {
+        } finally {
             setLoading(false);
         }
-    }
+    };
     return (
-        <form className='m-5 flex flex-col' onSubmit={handleSubmit}>
-            <input className='m-1 text-black border-1 rounded border-black' required
-                type = 'text'
-                value = {firstName}
+        <form className="m-5 flex flex-col" onSubmit={handleSubmit}>
+            <input
+                className="m-1 text-black border-1 rounded border-black"
+                required
+                type="text"
+                value={firstName}
                 onChange={(e) => setFirst(e.target.value)}
-                placeholder = "First name"
+                placeholder="First name"
             />
-            <input className='m-1 text-black border-1 rounded border-black' required
-                type = 'text'
-                value = {lastName}
+            <input
+                className="m-1 text-black border-1 rounded border-black"
+                required
+                type="text"
+                value={lastName}
                 onChange={(e) => setLast(e.target.value)}
-                placeholder = "Last name"
+                placeholder="Last name"
             />
-            <input className='m-1 text-black border-1 rounded border-black' required
-                type = 'email'
-                value = {email}
+            <input
+                className="m-1 text-black border-1 rounded border-black"
+                required
+                type="email"
+                value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                placeholder = "Email"
+                placeholder="Email"
             />
-            <input className='m-1 text-black border-1 rounded border-black' required
-                type = 'password'
-                value = {password}
+            <input
+                className="m-1 text-black border-1 rounded border-black"
+                required
+                type="password"
+                value={password}
                 onChange={(e) => setPass(e.target.value)}
-                placeholder = "Password (10+ characters)"
+                placeholder="Password (10+ characters)"
             />
             <div className="m-1 p-2 text-sm">
                 <ul>
-                    <li style={{ color: validation.minLength ? 'green' : 'red' }}> At least 10 characters</li>
-                    <li style={{ color: validation.hasLowercase ? 'green' : 'red' }}> A lowercase letter</li>
-                    <li style={{ color: validation.hasUppercase ? 'green' : 'red' }}>An uppercase letter</li>
-                    <li style={{ color: validation.hasNumber ? 'green' : 'red' }}> A number</li>
-                    <li style={{ color: validation.hasSpecialChar ? 'green' : 'red' }}>A special character (!@#$...)</li>
+                    <li
+                        style={{
+                            color: validation.minLength ? "green" : "red",
+                        }}
+                    >
+                        {" "}
+                        At least 10 characters
+                    </li>
+                    <li
+                        style={{
+                            color: validation.hasLowercase ? "green" : "red",
+                        }}
+                    >
+                        {" "}
+                        A lowercase letter
+                    </li>
+                    <li
+                        style={{
+                            color: validation.hasUppercase ? "green" : "red",
+                        }}
+                    >
+                        An uppercase letter
+                    </li>
+                    <li
+                        style={{
+                            color: validation.hasNumber ? "green" : "red",
+                        }}
+                    >
+                        {" "}
+                        A number
+                    </li>
+                    <li
+                        style={{
+                            color: validation.hasSpecialChar ? "green" : "red",
+                        }}
+                    >
+                        A special character (!@#$...)
+                    </li>
                 </ul>
             </div>
-            <input className='m-1 text-black border-1 rounded border-black' required
-                type = 'password'
-                value = {confirmPassword}
+            <input
+                className="m-1 text-black border-1 rounded border-black"
+                required
+                type="password"
+                value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
-                placeholder = "Confirm password"
+                placeholder="Confirm password"
             />
             <span className="m-1 text-black border-1 rounded border-black">
                 Request Seller Account?&nbsp;
@@ -168,14 +224,14 @@ const SignUp = () => {
             {error && <p className="m-1 text-red-500">{error}</p>}
             &nbsp;&nbsp;
             <button
-                className='m-1 text-black border-2 rounded border-black disabled:opacity-50'
-                type='submit'
+                className="m-1 text-black border-2 rounded border-black disabled:opacity-50"
+                type="submit"
                 disabled={loading}
             >
-            {loading ? 'Creating Account...' : 'Confirm'}
+                {loading ? "Creating Account..." : "Confirm"}
             </button>
         </form>
-    )
-}
+    );
+};
 
-export default SignUp
+export default SignUp;
--- thrift-u/functions/index.js
+++ thrift-u/functions/index.js
@@ -7,10 +7,13 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
+const { setGlobalOptions } = require("firebase-functions/v2");
+const { onRequest } = require("firebase-functions/https");
+const { onCall } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const { FieldValue } = require("firebase-admin/firestore");
 const serviceAccount = require("./serviceAccountKey.json");
 
 // For cost control, you can set the maximum number of containers that can be
@@ -23,17 +26,169 @@ const serviceAccount = require("./serviceAccountKey.json");
 // functions should each use functions.runWith({ maxInstances: 10 }) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-setGlobalOptions({maxInstances: 10});
+setGlobalOptions({ maxInstances: 10 });
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
 });
 
+const db = admin.firestore();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const roles = {
+    seller: 1,
+    buyer: 2,
+    admin: 0,
+};
+
+// assign customclaims role to user on signup
+
+exports.registerUser = functions.https.onCall(async (data, context) => {
+    const { email, password, role, firstName, lastName } = data;
+    console.log("Data:", {
+        email,
+        password,
+        role,
+        firstName,
+        lastName,
+    });
+
+    // if (!["buyer", "seller"].includes(role)) {
+    //     throw new functions.https.HttpsError(
+    //         "invalid-argument",
+    //         "A valid role ('buyer' or 'seller') must be provided.",
+    //     );
+    // }
+
+    try {
+        const userRecord = await admin.auth().createUser({
+            email: email,
+            password: password,
+            displayName: `${firstName} ${lastName}`,
+        });
+        // create user document in firestore
+        await db
+            .collection("User")
+            .doc(userRecord.uid)
+            .set({
+                email: email,
+                firstName: firstName,
+                lastName: lastName,
+                accessLevel: role === "seller" ? roles.seller : roles.buyer,
+                dateCreated: FieldValue.serverTimestamp(),
+                deletedAt: null,
+            });
+
+        let claims = {};
+        if (role === "seller") {
+            claims = { role: "seller", status: "pending_seller" };
+            // create seller document in firestore
+            await db.collection("Seller").doc(userRecord.uid).set({
+                UserID: userRecord.uid,
+                banned: false,
+                validated: false,
+                Flags: 0,
+            });
+
+            // create an application document in firestore for admins
+            await db.collection("sellerApplications").doc(userRecord.uid).set({
+                email: email,
+                firstName: firstName,
+                lastName: lastName,
+                status: "pending",
+                submittedAt: FieldValue.serverTimestamp(),
+                deletedAt: null,
+            });
+            // db document for buys will be done in seperate function
+        } else {
+            await db.collection("Buyer").doc(userRecord.uid).set({
+                UserID: userRecord.uid,
+                banned: false,
+                address: null,
+                city: null,
+                state: null,
+                zip: null,
+                numOrders: 0,
+            });
+            claims = { role: "buyer" };
+        }
+        // set the custom claims on the user's account
+        await admin.auth().setCustomUserClaims(userRecord.uid, claims);
+
+        return { uid: userRecord.uid, message: "User created successfully." };
+    } catch (error) {
+        if (error.code === "auth/email-already-exists") {
+            throw new functions.https.HttpsError(
+                "already-exists",
+                "This email is already in use.",
+            );
+        }
+
+        console.error("Error creating user:", error);
+        throw new functions.https.HttpsError(
+            "internal",
+            "An unexpected error occurred during registration.",
+        );
+    }
+});
+
+exports.approveSeller = functions.https.onCall(async (data, context) => {
+    if (context.auth.token.role !== "admin") {
+        throw new functions.https.HttpsError(
+            "permission-denied",
+            "Only an admin can change user roles.",
+        );
+    }
+
+    const { uid } = data;
+
+    if (!uid || typeof uid !== "string" || uid.length === 0) {
+        // check if argument is valid string
+        throw new functions.https.HttpsError(
+            "invalid-argument",
+            "A valid user ID (UID) must be provided.",
+        );
+    }
+
+    try {
+        const userRecord = await admin.auth().getUser(uid);
+        const customClaims = userRecord.customClaims || {};
+
+        if (
+            customClaims.role !== "seller" ||
+            customClaims.status !== "pending_seller"
+        ) {
+            // check if uid is a seller
+            throw new functions.https.HttpsError(
+                "failed-precondition",
+                "This user is not a pending seller and cannot be approved.",
+            );
+        }
+
+        const newClaims = {
+            // merging old custom claims with new status
+            ...customClaims,
+            status: "approved_seller",
+        };
+
+        await admin.auth().setCustomUserClaims(uid, newClaims);
+
+        await db.collection("sellerApplications").doc(uid).update({
+            // update database field for seller application
+            status: "approved",
+            approvedAt: FieldValue.serverTimestamp(),
+        });
+
+        return {
+            message: `Success! User ${uid} has been approved as a seller.`,
+        };
+    } catch (error) {
+        console.error("Error setting user role:", error);
+        throw new functions.https.HttpsError(
+            "internal",
+            "Failed to update user role.",
+        );
+    }
+});
