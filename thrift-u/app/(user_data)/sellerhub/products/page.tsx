608504b58aafbaee6a4e07a5bccac3fdaa921c61
TagReaper <coxlucas2022@gmail.com>
2025-10-23 02:17:51 -0500

    Temporary display of items in the sellerhub

--- thrift-u/app/(user_data)/sellerhub/products/page.tsx
+++ thrift-u/app/(user_data)/sellerhub/products/page.tsx
@@ -1,11 +1,21 @@
-import ItemListing from '../../../../components/productHandler/itemListing'
+import CompactItemListing from '../../../../components/productHandler/itemListingCompact'
 
 export default function ProductsPage() {
   return (
     <div>
       <h1>Products</h1>
       <p>Manage your listed products here.</p>
-      <ItemListing image={'/Graphics/ImagePlaceholder.jpg'} price={2.99} productName={'Picture'} sellerName={'John'} quantity={1}/>
+      <div className=" grid grid-cols-5">
+        <CompactItemListing image={'/Graphics/QBClubFootball.png'} price={89.99} productName={'Quarterback Club Signed Football'} quantity={1}/>
+        <CompactItemListing image={'/Graphics/QBClubFootball.png'} price={4.99} productName={'Quarterback Club Signed Football'} quantity={0}/>
+        <CompactItemListing image={'/Graphics/QBClubFootball.png'} price={4.99} productName={'Quarterback Club Signed Football'} quantity={0}/>
+        <CompactItemListing image={'/Graphics/QBClubFootball.png'} price={89.99} productName={'Quarterback Club Signed Football'} quantity={1}/>
+        <CompactItemListing image={'/Graphics/QBClubFootball.png'} price={4.99} productName={'Quarterback Club Signed Football'} quantity={0}/>
+        <CompactItemListing image={'/Graphics/QBClubFootball.png'} price={4.99} productName={'Quarterback Club Signed Football'} quantity={0}/>
+        <CompactItemListing image={'/Graphics/QBClubFootball.png'} price={89.99} productName={'Quarterback Club Signed Football'} quantity={1}/>
+        <CompactItemListing image={'/Graphics/QBClubFootball.png'} price={4.99} productName={'Quarterback Club Signed Football'} quantity={0}/>
+        <CompactItemListing image={'/Graphics/QBClubFootball.png'} price={4.99} productName={'Quarterback Club Signed Football'} quantity={0}/>
+      </div>
     </div>
   );
 }
