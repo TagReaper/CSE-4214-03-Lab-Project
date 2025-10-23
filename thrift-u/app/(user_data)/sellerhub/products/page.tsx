cec92cec9479c39eff04d920e7ef176d05c8d5ec
TagReaper <coxlucas2022@gmail.com>
2025-10-23 18:21:47 -0500

    seller accessLevel Verification

--- thrift-u/app/(user_data)/sellerhub/products/page.tsx
+++ thrift-u/app/(user_data)/sellerhub/products/page.tsx
@@ -1,8 +1,25 @@
+'use client'
+
 import Image from 'next/image'
+import { useEffect } from 'react';
+import { useRouter } from "next/navigation";
+import {useAuthState} from "react-firebase-hooks/auth";
+import FireData from "../../../../firebase/clientApp";
 import SellerItemList from '../../../../components/seller/seller_listItems'
 
 export default function ProductsPage() {
-  const [user] = useAuthState(FireData.auth);
+    const router = useRouter();
+    //Check sign-in state
+    const [user] = useAuthState(FireData.auth);
+    //Pushed to home if they are signed in
+    useEffect(() => {
+      if(user){
+        if (user.uid != 'beacon') {
+            router.push("/");
+        }
+      }
+    }, [user, router]);
+
   return (
     <div>
       <SellerItemList sellerId={''}/>
