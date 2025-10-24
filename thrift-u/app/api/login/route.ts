72565a1bae9efbc5b3105f9e0fe19e2a9523cfd3
TagReaper <coxlucas2022@gmail.com>
2025-10-24 00:53:31 -0500

    Created an API route to create cookies at login

---
+++ thrift-u/app/api/login/route.ts
@@ -0,0 +1,19 @@
+import {headers, cookies} from 'next/headers'
+
+export async function POST() {
+    const headersList = await headers();
+    const auth = headersList.get("Authorization");
+    const altId = headersList.get("AltID");
+    const access = headersList.get("AccessLevel");
+
+    const cookieStore = await cookies();
+    cookieStore.set("idToken", `${auth}`);
+    cookieStore.set("altID", `${altId}`);
+    cookieStore.set("accessLevel", `${access}`);
+
+    console.log(cookieStore);
+
+    return new Response('success',{
+        status: 200
+    })
+}
