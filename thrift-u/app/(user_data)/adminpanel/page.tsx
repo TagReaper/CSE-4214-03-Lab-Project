73b2ff8f16270f54962112181424daf7009f38ee
revcane <xrengamer@gmail.com>
2025-10-23 23:49:02 -0500

    Updated page for admin product approval list

--- thrift-u/app/(user_data)/adminpanel/page.tsx
+++ thrift-u/app/(user_data)/adminpanel/page.tsx
@@ -1,10 +1,20 @@
-import ListUsers from '../../../components/admin_UserList'
+import ListUsers from '../../../components/adminpanel/admin_UserList'
+import PendingProductList from '../../../components/adminpanel/adminProductList'
 
 export default function AdminPage() {
     return (
-        <div className='center'>
-        Admin Control Page
-        <ListUsers />
+        <div className="p-4">
+            <h1 className="text-2xl font-bold text-center mb-4">
+                Admin Control Page
+            </h1>
+            <div className="flex flex-row gap-4">
+                <div className="flex-1">
+                <ListUsers />
+                </div>
+                <div className="flex-1">
+                <PendingProductList />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
