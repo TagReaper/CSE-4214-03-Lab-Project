5a267224f334db613cdb110e094f7af5e5f2d995
TagReaper <coxlucas2022@gmail.com>
2025-11-01 21:18:57 -0500

    Completed the code for checkout in the buytrain

--- thrift-u/components/buyTrain/actions.js
+++ thrift-u/components/buyTrain/actions.js
@@ -1,48 +1,80 @@
 "use server";
-import { getDoc, getDocs, doc, updateDoc, addDoc, setDoc} from "@firebase/firestore";
+import { getDoc, getDocs, collection, doc, updateDoc, addDoc, setDoc} from "@firebase/firestore";
 import FireData from "@/firebase/clientApp";
-import { verifyRole } from "@/lib/auth";
+import { getAuthUser, verifyRole } from "@/lib/auth";
 
 export async function checkout(Cart, Address, Payment) {
-    /*
-    get Inventory
-    Items = []
-    orderSum = 0
-    try{
-        if(!Payment) -> throw Error(Payment Declined)
-        if Payment.default -> setDoc to Payment as userId: Payment, deletedAt: ""
+    const token = await getAuthUser()
 
-        for each cartItems
-            item = Inventory.find(obj => obj.id == cartItems[index].itemId)
-            if !item -> throw Error(ItemId not found)
-            item.orderqty = CartItems[index].qty
-            item.sum = item.price * item.orderqty
-            orderSum += item.sum
-            Items.push(item)
-
-        orderRef=addDoc to Orders : cart.length, orderSum, date, Address, deletedAt: ""
-
-        for each item in Items
-            get Seller from item.sellerId
-            income = item.sum + Seller.income
-            unclaimedIncome = item.sum + Seller.unclaimedIncome
-            update Seller at item.sellerId: seller.income & seller.unclaimedIncome
-            addDoc to OrderItems : OrderRef.id, item.id, item.orderqty, status:"pending", TrackingNumber: "", date, deletedAt: ""
-
-        BuyerRef = get buyer from user.id
-        update Buyer at user.id: numOrders: BuyerRef.numOrders+1, cart: []
-
-        return true
-
-
-    } catch(error) {
-        console.error("Checkout failed:", error);
-        throw error
-    }
-
-    */
     if (verifyRole("Buyer")){
-        return true
+        const Inventory = await getDocs(collection(FireData.db, "Inventory"))
+        const serverTime = new Date();
+        var Items = []
+        var orderSum = 0
+        try{
+            if(!Payment) {throw Error("Payment Declined")}
+            if (Payment.default){
+                await setDoc(doc(FireData.db, "Payment", token.user_id), {
+                    cardNumber: Payment.cardNumber,
+                    exp: Payment.exp,
+                    cvc: Payment.cvc,
+                    firstName: Payment.firstName,
+                    lastName: Payment.lastName
+                })
+            }
+
+            for (let index = 0; index < Cart.length; index++) {
+                const item = Inventory.find(obj => obj.id == Cart[index].itemId)
+                if (!item) {throw Error("ItemId not found")}
+                item.id = Cart[index].itemId
+                item.orderqty = Cart[index].qty
+                item.sum = item.price * item.orderqty
+                orderSum += item.sum
+                Items.push(item)
+            }
+
+            orderRef = await addDoc(collection(FireData.db, "Orders"), {
+                quantity : Cart.length,
+                cardUsed: Payment.cardNumber % 10000,
+                price: orderSum,
+                date: serverTime.toLocaleString(),
+                address: Address.address,
+                city: Address.city,
+                state: Address.state,
+                zip: Address.zip,
+                fulfilledAt: "",
+            })
+
+            for (let index = 0; index < Items.length; index++) {
+                const seller = await getDoc(doc(FireData.db, "Seller", Items[index].sellerId))
+                let income = Items[index] + seller.income
+                let unclaimedIncome = Items[index].sum + seller.unclaimedIncome
+                await updateDoc(doc(FireData.db, "Seller", Items[index].sellerId), {
+                    income: income,
+                    unclaimedIncome: unclaimedIncome,
+                })
+                await addDoc(collection(FireData.db, "Orders"), {
+                    orderId: orderRef.id,
+                    itemId: Items[index].id,
+                    quantity: Items[index].orderqty,
+                    status:"pending",
+                    trackingNumber: "",
+                    dateAccepted: ""
+                })
+            }
+
+            const Buyer = await getDoc(doc(FireData.db, "Buyer", token.user_id))
+
+            await await updateDoc(doc(FireData.db, "Buyer", token.user_id), {
+                    numOrders: Buyer.numOrders + 1,
+                    cart: [],
+                })
+
+            return true
+        } catch(error) {
+            console.error("Checkout failed:", error);
+            throw error
+        }
     } else {
         return false
     }
