84438fbaa405478b08bd548f39b50e94b066644a
TagReaper <coxlucas2022@gmail.com>
2025-11-06 18:43:13 -0600

    Change cartQTY to cart as an array

--- thrift-u/components/auth/signUp.js
+++ thrift-u/components/auth/signUp.js
@@ -112,7 +112,7 @@ const SignUp = () => {
           state: "",
           zip: "",
           numOrders: 0,
-          cartQTY: 0,
+          cart: [],
           deletedAt: "",
         });
       }
