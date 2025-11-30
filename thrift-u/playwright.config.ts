57c6652b985b056429e2e724a875a92f12bcd1d8
revcane <xrengamer@gmail.com>
2025-11-30 04:08:09 -0600

    added dotenv package, inital auth setup for testing

--- thrift-u/playwright.config.ts
+++ thrift-u/playwright.config.ts
@@ -1,18 +1,14 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// load test environment variables
+dotenv.config({ path: ".env.test" });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './__tests__/playwright',
+  testDir: "./__tests__/playwright",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,58 +18,66 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
+    // setup project
+    { name: "setup", testMatch: /.*\.setup\.ts/ },
+
+    // tests that require buyer authentication
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "buyer",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: "playwright/.auth/buyer.json",
+      },
+      dependencies: ["setup"],
+      testMatch: /.*buyer.*\.spec\.ts/,
     },
 
+    // tests that require seller authentication
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "seller",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: "playwright/.auth/seller.json",
+      },
+      dependencies: ["setup"],
+      testMatch: /.*seller.*\.spec\.ts/,
     },
 
+    // tests that require admin authentication
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "admin",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: "playwright/.auth/admin.json",
+      },
+      dependencies: ["setup"],
+      testMatch: /.*admin.*\.spec\.ts/,
     },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    // tests that don't require authentication
+    {
+      name: "unauthenticated",
+      use: { ...devices["Desktop Chrome"] },
+      testMatch: /.*\/(login|signup)\.spec\.ts/,
+    },
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
